ad0ebb51d1cf933ca51f5ffe480fb21bf4421afd
tarakish <akr.3838soccer@gmail.com>
2021-10-10 04:17:03 +0900

    Add: actors_controllerを追加

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,6 @@ module RunteqNormal
     config.generators.system_tests = nil
 
     config.generators do |g| 
-
       g.skip_routes true 
       g.assets false 
       g.helper false 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  
+
+  resources :actors, onli: %i[index show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
