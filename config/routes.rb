672571ed3a7f168f35863dee8b00f722cfa024ae
tarakish <akr.3838soccer@gmail.com>
2021-10-13 05:31:22 +0900

    Add:bootstrapのバージョンを元に戻す。アクター一覧と詳細の雛形作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   root 'staticpages#top'
-  resources :actors, onli: %i[index show]
+  resources :actors, only: %i[index show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
