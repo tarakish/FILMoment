8528cbddec036117d5659a825cdec98cadad085e
tarakish <akr.3838soccer@gmail.com>
2021-10-14 05:40:02 +0900

    Add: 投稿機能と投稿フォーム追加
    ・posts_controllerにcreateアクションを追加
    ・役者詳細ページに投稿フォームを投稿一覧のパーシャルを追加
    ・エラーメッセージ表示用のパーシャルをshared/に追加し、add_flash_typesをapplicationcontrollerに追記

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,17 @@
 class PostsController < ApplicationController
   def create
+    @actor = Actor.find(params[:actor_id])
+    @post = @actor.posts.build(post_params)
+    if @post.save
+      redirect_to actor_path(@actor), success: '想いを追加しました'
+    else
+      flash.now[:danger] = 'ごめんなさい、想いを追加出来ませんでした'
+      render actor_path(@actor)
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:film_title, :scene, :author)
   end
 end
