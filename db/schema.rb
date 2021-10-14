5f4bbf554d41365302efefc189cfde793f6cb588
tarakish <akr.3838soccer@gmail.com>
2021-10-14 09:56:58 +0900

    Add: likesモデルとコントローラの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_004959) do
+ActiveRecord::Schema.define(version: 2021_10_14_004245) do
 
   create_table "actors", force: :cascade do |t|
     t.string "name", null: false
     t.text "wiki_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
